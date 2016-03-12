92814f277d5ba79416c721c806751acba14ec20d
Kenji Ohtsuka <kok.fdcm@gmail.com>
2016-03-12 21:43:29 +0900

    add aws sdk

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
 ./node_modules/.bin/jade src/jade --out electron-src/html
 #./node_modules/.bin/tsc --outDir electron-src/js -p src/ts
 ./node_modules/.bin/coffee -c -o electron-src/js src/coffee
+./node_modules/.bin/node-sass -o electron-src/css src/sass
